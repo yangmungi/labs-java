61146a334ded20ad38a68c866cf4e25b3f727c63
yangmungi@gmail.com <>
2014-03-15 14:47:48 -0700

    Finishing permuter.

--- src/main/java/com/yangmungi/labs/Permuter.java
+++ src/main/java/com/yangmungi/labs/Permuter.java
@@ -1,5 +1,6 @@
 package com.yangmungi.labs;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -7,60 +8,157 @@ import java.util.List;
  *
  * @author Yangmun
  */
-public class Permuter {
-    private final Collection origin;    
-    
-    private Collection<List> collection;
-    
-    // This list should have supremely optimal add()
-    private ListBuilder writeListBuilder;
-    
-    // This list should have supremely optimal subList()     
-    private ListBuilder readListBuilder;
-    
+public class Permuter {    
     public Permuter(Collection origin) {
         this.origin = origin;
     }
+
+    //
+    //  Optional Factories
+    //
+    
+    private static class DefaultListBuilder implements Builder<List> {
+        public List build() {
+            return new ArrayList();
+        }
+    }
+    
+    private static class DefaultCollectionOfListBuilder implements Builder<Collection<List>> {
+        public Collection<List> build() {
+            return new ArrayList();
+        }
+    }
+    
+    protected static Builder<List> getDefaultListBuilder() {
+        return new DefaultListBuilder();
+    }
+    
+    protected static Builder<Collection<List>> getDefaultCollectionOfListBuilder() {
+        return new DefaultCollectionOfListBuilder();
+    }
+    
+    private final Collection origin;    
+    
+    // Resultant object
+    private Collection<List> collection;
+    
+    private Builder<Collection<List>> collectionOfListBuilder;
+    
+    // This list should have supremely optimal add()
+    private Builder<List> writeListBuilder;
+    
+    // This list should have supremely optimal subList()     
+    private Builder<List> readListBuilder;
+    
+    public void setWriteListBuilder(Builder<List> builder) {
+        this.writeListBuilder = builder;
+    }
+    
+    public final Builder<List> getWriteListBuilder() {
+        if (this.writeListBuilder == null) {
+            this.writeListBuilder = getDefaultListBuilder();
+        }
+        
+        return this.writeListBuilder;
+    }
+    
+    public void setReadListBuilder(Builder<List> builder) {
+        this.readListBuilder = builder;
+    }
+    
+    public final Builder<List> getReadListBuilder() {
+        if (this.readListBuilder == null) {
+            this.readListBuilder = getDefaultListBuilder();
+        }
+        
+        return this.readListBuilder;
+    }
+    
+    public void setCollectionOfListBuilder(Builder<Collection<List>> builder) {
+        this.collectionOfListBuilder = builder;
+    }
+    
+    public final Builder<Collection<List>> getCollectionOfListBuilder() {
+        if (this.collectionOfListBuilder == null) {
+            this.collectionOfListBuilder = getDefaultCollectionOfListBuilder();
+        }
+        
+        return this.collectionOfListBuilder;
+    }
+    
+    public void setCollection(Collection<List> collection) {
+        this.collection = collection;
+    }
+    
+    public Collection<List> getCollection() {
+        return this.collection;
+    }
     
     /**
-     * Returns a Collection of Collections
-     * @return 
+     * Returns all possible permutations of a List consisting of
+     *  the objects from Collection origin
      */
-    public Collection<List> permute() {
+    public void execute() {
         if (this.collection == null) {
-            throw new java.lang.IllegalStateException("Collection not provided");
-        }
+            throw new IllegalStateException("Collection not provided");
+        }         
+        
+        Collection resultant = this.collection;
         
         // Only needed for the interface?
         List originList = this.buildReadList();
         originList.addAll(this.origin);
         
-        int originListSize = originList.size();
+        int originListSize = originList.size();            
         int originListSizeMinusOne = originListSize - 1;
-        
+
         List newSubList;
-        
-        for (int i = 0; i < originListSize; i++) {
-            
+
+        for (int i = 0; i < originListSize; i++) {            
             newSubList = this.buildWriteList();
             if (i > 0) {
                 newSubList.addAll(originList.subList(0, i));
             }
-            
+
             if (i < originListSizeMinusOne) {
                 newSubList.addAll(originList.subList(i + 1, originListSize));
             }
-                    
+
+            Permuter recursive = this.buildPermuter(newSubList);
+            
+            recursive.setCollectionOfListBuilder(this.getCollectionOfListBuilder());
+            recursive.setCollection(this.buildCollection());
+            recursive.setReadListBuilder(this.getReadListBuilder());
+            recursive.setWriteListBuilder(this.getWriteListBuilder());
+            
+            recursive.execute();
+            
+            Collection<List> recursiveResult = recursive.getCollection();
+            
+            if (recursiveResult.isEmpty()) {
+                recursiveResult.add(this.buildWriteList());
+            }
+            
+            for (List subList : recursiveResult) {
+                subList.add(0, originList.get(i));
+                resultant.add(subList);
+            }  
         }   
-        
-        return null;
     }
     
+    protected Permuter buildPermuter(Collection collection) {
+        return new Permuter(collection);
+    }
+    
+    private Collection<List> buildCollection() {
+        return this.getCollectionOfListBuilder().build();
+    }
+        
     private List buildReadList() {
-        return this.readListBuilder.build();
+        return this.getReadListBuilder().build();
     }
  
     private List buildWriteList() {
-        return this.writeListBuilder.build();
+        return this.getWriteListBuilder().build();
     }
 }
---
+++ src/test/java/com/yangmungi/labs/PermuterTest.java
@@ -0,0 +1,58 @@
+package com.yangmungi.labs;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple Permuter.
+ */
+public class PermuterTest
+        extends TestCase {
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public PermuterTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(PermuterTest.class);
+    }
+
+    /**
+     * Rigorous Test :-)
+     */
+    public void testApp() {
+        int targetSize = 7;
+        ArrayList<Integer> source = new ArrayList<Integer>(targetSize);
+        for (int i = 1; i <= targetSize; i++) {
+            source.add(i);
+        }
+        
+        int testedSize = 1;
+        for (int i = 2; i <= targetSize; i++) {
+            testedSize *= i;
+        }
+        
+        Permuter p = new Permuter(source);
+        
+        p.setCollection(new ArrayList());
+        
+        p.execute();
+        
+        Collection result = p.getCollection();
+        
+        assertEquals(testedSize, result.size());
+        
+        //System.out.println(result);
+    }
+}
